044e7a4a549e2d990b0f725b7f8fbf32ae7ba280
drknsblct <drknsblct@gmail.com>
2021-10-05 04:01:40 +0300

    project init

---
+++ generate_text.py
@@ -0,0 +1,8 @@
+subs = []  # add your subreddits
+users = []  # add users you follow
+
+with open('new.txt', 'w') as f:
+    for sub in subs:
+        f.write(f'https://www.reddit.com/r/{i}\n')
+    for user in users:
+        f.write(f'https://www.reddit.com/{i}\n')
---
+++ main.py
@@ -0,0 +1,31 @@
+import secrets
+from selenium import webdriver
+from time import sleep
+
+PATH = '/Users/blackout/reddit_subs/chromedriver'  # Path for chromedriver location
+driver = webdriver.Chrome(PATH)
+
+driver.get('https://www.reddit.com/login/?dest=https%3A%2F%2Fwww.reddit.com%2F')
+
+# Credentials and Login
+username = driver.find_element_by_id('loginUsername')
+password = driver.find_element_by_id('loginPassword')
+username.send_keys(secrets.username)  # Enter your username
+password.send_keys(secrets.password)  # Enter your password
+driver.find_element_by_xpath('/html/body/div/main/div[1]/div/div[2]/form/fieldset[5]/button').click()
+
+# to bypass chrome popup notification
+sleep(5)
+
+with open('subreddits.txt', 'r') as r:
+    for line in r:
+        driver.get(line)
+        sleep(2)
+        if line[23] == 'r':
+            driver.find_element_by_xpath(
+                '/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[1]/div/div[1]/div/div[2]/button').click()
+        else:
+            driver.find_element_by_xpath(
+                '/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[4]/div[2]/div/div[1]/div/div[5]/div[1]/button').click()
+    driver.quit()
+
