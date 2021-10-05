177627ed3aa83d0f1126780f1644787d69ef01b2
drknsblct <57183092+drknsblct@users.noreply.github.com>
2021-10-05 04:11:52 +0300

    Update generate_text.py

--- generate_text.py
+++ generate_text.py
@@ -1,7 +1,7 @@
 subs = []  # add your subreddits
 users = []  # add users you follow
 
-with open('new.txt', 'w') as f:
+with open('subreddits.txt', 'w') as f:
     for sub in subs:
         f.write(f'https://www.reddit.com/r/{i}\n')
     for user in users:
