0387ed065f92109ed8300c36a40f52e64e1d481f
drknsblct <57183092+drknsblct@users.noreply.github.com>
2021-10-05 04:18:25 +0300

    Update generate_text.py

--- generate_text.py
+++ generate_text.py
@@ -3,6 +3,6 @@ users = []  # add users you follow
 
 with open('subreddits.txt', 'w') as f:
     for sub in subs:
-        f.write(f'https://www.reddit.com/r/{i}\n')
+        f.write(f'https://www.reddit.com/r/{sub}\n')
     for user in users:
-        f.write(f'https://www.reddit.com/{i}\n')
+        f.write(f'https://www.reddit.com/{user}\n')
