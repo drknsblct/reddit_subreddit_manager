01402c8e2cf751868baea37a590caaa6838c49e2
drknsblct <drknsblct@gmail.com>
2021-10-23 19:54:33 +0300

    main program now uses txt file to gerenate urls

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import secrets
 from random import randint
 from selenium import webdriver
 from time import sleep
-from generate_urls import subs_with_urls
 
 PATH = './chromedriver'
 
@@ -10,28 +9,41 @@ driver = webdriver.Chrome(PATH)
 
 driver.get('https://www.reddit.com/login/?dest=https%3A%2F%2Fwww.reddit.com%2F')
 
-# Credentials and Login
+# credentials and login
 username = driver.find_element_by_id('loginUsername')
 password = driver.find_element_by_id('loginPassword')
 username.send_keys(secrets.username)
 password.send_keys(secrets.password)
 driver.find_element_by_xpath('/html/body/div/main/div[1]/div/div[2]/form/fieldset[5]/button').click()
 
+subreddit_xpath = '/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[1]/div/div[1]/div/div[2]/button'
+user_xpath = '/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[4]/div[2]/div/div[1]/div/div[5]/div[1]/button'
+
 # to bypass chrome popup notification
 sleep(5)
 
-for line in subs_with_urls:
-    driver.get(line)
-    # you can change the range if you want it to go faster
-    # but be careful to not spam the reddit server with too many requests!
-    # sleep(2)
-    sleep(randint(5, 20))
+with open('subs.txt', 'r') as f:
+    for line in f:
+        if line == '\n':
+            continue
 
-    if line[23] == 'r':
-        driver.find_element_by_xpath(
-            '/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[1]/div/div[1]/div/div[2]/button').click()
-    else:
-        driver.find_element_by_xpath(
-            '/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[4]/div[2]/div/div[1]/div/div[5]/div[1]/button').click()
+        if line[:2] == 'u/':
+            driver.get(f'https://www.reddit.com/{line}')
+        else:
+            driver.get(f'https://www.reddit.com/r/{line}')
+
+        # you can change the sleep range if you want it to go faster
+        # but be careful to not spam the reddit server with too many requests!
+        # sleep(randint(5, 20))
+        sleep(2)
+
+        if line[:2] == 'u/':
+            driver.find_element_by_xpath(user_xpath).click()
+        else:
+            driver.find_element_by_xpath(subreddit_xpath).click()
+
     sleep(2)
 driver.quit()
+
+
+
--- readme.md
+++ readme.md
@@ -14,28 +14,16 @@
 - Type or paste [reddit.com/subreddits](reddit.com/subreddits) on the address bar.
 
 
-- Paste the following snippet on the address bar and press `Enter`:
+- Paste the following [snippet](https://timvisee.com/blog/list-export-your-subreddits/) on the address bar and press `Enter`:
   ```javascript
   javascript:$('body').replaceWith('<body>'+$('.subscription-box').find('li').find('a.title').map((_, d) => $(d).text()).get().join("<br>")+'</body>');javascript.void()
   ``` 
-  ##### Code snippet was from [here](https://timvisee.com/blog/list-export-your-subreddits/).
 
-- Copy the list of all the subreddits and users and paste it in `generate_urls.py` on the subs list.
+- Copy all and paste it in a text file named `subs.txt`. Add the text file to the same folder as the program.
 
-> #### Follow these steps to mass add elements to list:
->
-> Click on the left of the first element and then press `Ctrl` + `Alt` and drag mouse from left to right and then down to the end of list to select all elements.
->
-> Add quotes (`" "` or `' '`)
->
-> Click &#8594; key on keyboard
->
-> Add `,`
->
-> Delete comma on the right of last element
 
 - Create file named `secrets.py` and add your username/password in variables with the same name. Use the Reddit account in which the subreddits will be transfered!
 
 ### Disclaimer
 
-- Use this program with **caution!** I'm not held responsible if your account gets banned.
+- Use this program with **caution**, meaning don't spam the Reddit server with too many requests. I'm not held responsible if your account gets banned.
